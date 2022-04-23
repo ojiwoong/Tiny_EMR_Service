1f50c0e05b2c7c20b1ad7a4ffea769059a566bad
ojiwoong <ojiwoong@gmail.com>
2022-04-23 18:36:31 +0900

    chore: 웹 프로젝트 초기 구성
    Language : Java 11.0.10
    Build : gradle
    Packaging : Jar
    FrameWork : Spring Boot 2.6.7
    Dependencies : Spring Web, Spring Data JPA, H2 Database, Lombok,

---
+++ src/main/java/com/hdjunction/tinyERMService/TinyErmServiceApplication.java
@@ -0,0 +1,13 @@
+package com.hdjunction.tinyERMService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TinyErmServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TinyErmServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hdjunction/tinyERMService/TinyErmServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hdjunction.tinyERMService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TinyErmServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
